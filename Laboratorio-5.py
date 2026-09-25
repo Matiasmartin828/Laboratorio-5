8bf441dbfb469f9a5e36b18a7b904ae78f27ae2f
Matías Martin <matiasmartin828@gmail.com>
2026-09-25 12:44:52 -0300

    Archivo base

---
+++ Laboratorio-5.py
@@ -0,0 +1,11 @@
+# LABORATORIO 5
+# AUTORES MATÍAS MARTIN E IVO DI MARCO
+# LINK DEL REPOSITORIO DE GITHUB: https://github.com/Matiasmartin828/Laboratorio-5.git
+
+import tkinter as tk
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+
+
