021cf9aa2b9be039576c633a42f85cd50acf31fe
Matías Martin <matiasmartin828@gmail.com>
2026-09-25 14:52:02 -0300

    Estadísticas del archivo csv

--- Laboratorio-5.py
+++ Laboratorio-5.py
@@ -2,10 +2,15 @@
 # AUTORES MATÍAS MARTIN E IVO DI MARCO
 # LINK DEL REPOSITORIO DE GITHUB: https://github.com/Matiasmartin828/Laboratorio-5.git
 
-import tkinter as tk
+import tkinter as tk                                                                            #Librería para crear interfaces gráficas de usuario (GUI)
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
+dataframe = pd.read_csv('telemetria_nodo_iot.csv', parse_dates=['timestamp'])                   #Leo el archivo CSV y convierto la columna 'timestamp' a tipo datetime
+dataframe = dataframe.set_index('timestamp')                                                    #Establezco la columna 'timestamp' como índice
+print("Estadísticas descriptivas:\n")
+estadisticas = dataframe.describe().loc[['mean', 'min', 'max', 'std']]                          #Muestro las estadísticas descriptivas de las columnas numéricas del DataFrame, incluyendo la media, el valor mínimo, el valor máximo y la desviación estándar
+print(estadisticas, "\n")
 
-
+                                  
